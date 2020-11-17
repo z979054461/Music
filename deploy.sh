4c8532b88cf192d64fc058f0ead31d4733773e0e
yuetianxiang <979054461@qq.com>
2020-11-17 22:39:36 +0800

    更新PDF为图片

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:z979054461/Music.git master:gh-pages
+git push -f git@github.com:z979054461/Music.git main:gh-pages
 
 cd -
