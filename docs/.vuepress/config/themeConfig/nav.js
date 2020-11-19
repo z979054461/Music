0dba13db8bff1e904161afdd4397628926b80621
yuetianxiang <979054461@qq.com>
2020-11-19 21:05:02 +0800

    增加其他练习曲图片

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,28 +1,15 @@
 module.exports = {
   'zh': [
-    // {
-    //   text: '文档',
-    //   icon: 'reco-api',
-    //   items: [
-    //     { text: '数据结构', link: '/documents/01.数据结构/' },
-    //     { text: 'JS', link: '/documents/02.js/' },
-    //     { text: 'css', link: '/documents/03.css/' },
-    //     { text: 'vue', link: '/documents/04.vue/' },
-    //     { text: 'dataBase', link: '/documents/05.dataBase/' },
-    //     { text: '自动化测试', link: '/documents/07.自动化测试/' },
-    //     { text: '配置管理', link: '/documents/08.配置管理/' },
-    //     { text: 'linux', link: '/documents/09.linux/' },
-    //     { text: 'LeeCode', link: '/documents/10.LeeCode/' },
-    //   ]
-    // },
     {
       text: '目录',
       icon: 'reco-blog',
       items: [
-        { text: 'tutorial', link: '/categories/tutorial/'},//注意这里写目录
-        { text: 'practice', link: '/categories/practice/' },
+        { text: '进阶教程', link: '/categories/tutorial/'},//注意这里写目录
+        { text: '练习曲', link: '/categories/practice/' },
+        { text: '基础', link: '/categories/basic/' },
       ]
     },
+    { text: '标签', link: '/tag/', icon: 'reco-tag' },
     { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
     { text: 'GitHub', link: 'https://github.com/z979054461/Music', icon: 'reco-github' }
   ],
---
+++ docs/03.Ukulele练习曲/恋爱ing.md
@@ -0,0 +1,19 @@
+---
+title: 恋爱ing
+date: 2020-11-19
+lang: 'zh-CN'
+sidebar: 'auto'
+categories:
+ - practice
+tags: 
+  - ukulele 
+  - 白熊音乐 
+  - 弹唱 
+  - 恋爱ing
+  - 五月天
+location: HangZhou
+---
+
+![](/practice/弹唱_恋爱ing.png) 
+
+![](/practice/弹唱_恋爱ing2.png) 
